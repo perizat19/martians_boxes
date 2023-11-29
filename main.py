8202c1925914a35f01115010eb6761900e471a4e
perizat19 <150646975+perizat19@users.noreply.github.com>
2023-11-29 10:15:38 +0600

    5 commit

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def find_cargo(first_loc, second_loc, third_loc, first_weight, second_weight, th
     total_weight = 0
     total_ws = [first_weight, second_weight, third_weight]
     
-kilometer_mark = int(input(f"Enter the kilometer mark for box {i+1}: "))
-if cargo_locations[i] == kilometer_mark:
-                print("Cargo already found at this location!")
+    for i in range(3):
+        kilometer_mark = int(input(f"Enter the kilometer mark for box {i+1}: "))
+        if cargo_locations[i] == kilometer_mark:
+            print("Cargo is already found at this location!")
+            total_weight += total_ws[i]
+            print (total_weight)
+                
+            
